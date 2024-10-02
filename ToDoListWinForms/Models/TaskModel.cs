41718eab89713d11f4d291055089cb4a9b053b94
Arsen Burko <arsenburko67@gmail.com>
2024-10-02 14:39:34 +0200

    Convert to windows forms

---
+++ ToDoListWinForms/Models/TaskModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoListWinForms.Models
+{
+    internal class TaskModel
+    {
+        public string Task { get; set; } = "";
+        public bool IsCompleted { get; set; } = false;
+        public DateTime? CompleteDate { get; set; }
+    }
+}
